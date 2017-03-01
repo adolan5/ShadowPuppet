d4804179d433f512fd403cff3a1586265ef243a1
Andy D <then3rd3.14@gmail.com>
2017-03-01 00:12:55 -0700

    (I think) OpenGL is up and working, so long as it is installed on host machine. Successfully opened XBox360 wired controller.

--- makefile
+++ makefile
@@ -6,5 +6,8 @@ C_SRCS = test.cc
 all:
 	$(CC) $(C_Flags) -o $(EXE) $(C_SRCS) `sdl2-config --libs`
 	
+debug:
+	$(CC) $(C_Flags) -ggdb -o $(EXE) $(C_SRCS) `sdl2-config --libs`
+	
 clean:
 	rm -f $(EXE)
--- test.cc
+++ test.cc
@@ -3,15 +3,30 @@
 
 using namespace std;
 
+#define MAX_CONTROLLERS 4
+
 int main(int argc, char *argv[]){
-    if(SDL_Init(SDL_INIT_VIDEO) != 0){
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) != 0){
         SDL_Log("Unable to initialize SDL: %s\n", SDL_GetError());
     }
     
     SDL_Window *myWindow;
-    myWindow = SDL_CreateWindow("My window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    myWindow = SDL_CreateWindow("My window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    //An array of pointers to game controller structures
+    SDL_GameController *controller;
+    //The number of joysticks detected by SDL
+    int joysticks = SDL_NumJoysticks();
+    for(int joystickIndex = 0; joystickIndex < joysticks; ++joystickIndex){
+        if(!SDL_IsGameController(joystickIndex)){
+            continue;
+        }
+        cout << "Joystick at index: " << joystickIndex << " is a valid game controller\n";
+        controller = SDL_GameControllerOpen(joystickIndex);
+        break;
+    }
     SDL_Delay(3000);
     SDL_DestroyWindow(myWindow);
+    SDL_GameControllerClose(controller);
     SDL_Quit();
     return 0;
 }
