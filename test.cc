6398c3c07e7097fa57c3cf2d2eba1d23824d71ba
Andy D <then3rd3.14@gmail.com>
2017-03-04 13:00:58 -0700

    Added more to test.cc: Starting to look at rendering graphics and event polling. Added images directory: Place to hold sprites/backgrounds for the game, etc.

--- test.cc
+++ test.cc
@@ -13,14 +13,37 @@ int main(int argc, char *argv[]){
     //Pointers for our window and renderer, and controller
     SDL_Window *window;
     SDL_Renderer *renderer;
+    SDL_Surface *backgroundSurface;
+    SDL_Texture *background;
     SDL_GameController *controller;
+    //An event to check if we're still running
+    SDL_Event event;
     
     //Creating a window
     if(SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_OPENGL, &window, &renderer)){
-        SDL_Log("Unable to create window and renderer: %s\n", SDL_GetError());
+        cerr << argv[0] << ": ";
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to create window and renderer! Error: %s\n", SDL_GetError());
+        return 1;
     }
+    //Loading the background image as a surface
+    backgroundSurface = SDL_LoadBMP("./images/wht-marble.bmp");
+    if(!backgroundSurface){
+        cerr << argv[0] << ": ";
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't load backgroundSurface from bmp! Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    //Creating a texture from the above surface
+    background = SDL_CreateTextureFromSurface(renderer, backgroundSurface);
+    if(!background){
+        cerr << argv[0] << ": ";
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from background surface! Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    //Now we're done with the surface, so we can free it
+    SDL_FreeSurface(backgroundSurface);
     
-    //The number of joysticks detected by SDL
+    //Opening the gamepad if one is connected
+    bool gamepadConnected = false;
     int joysticks = SDL_NumJoysticks();
     for(int joystickIndex = 0; joystickIndex < joysticks; ++joystickIndex){
         if(!SDL_IsGameController(joystickIndex)){
@@ -28,12 +51,25 @@ int main(int argc, char *argv[]){
         }
         cout << "Joystick at index: " << joystickIndex << " is a valid game controller\n";
         controller = SDL_GameControllerOpen(joystickIndex);
+        gamepadConnected = true;
         break;
     }
-    SDL_Delay(3000);
-    SDL_DestroyRenderer(renderer);
+    
+    while(1){
+        SDL_PollEvent(&event);
+        if(event.type == SDL_QUIT){
+            break;
+        }
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, background, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
     SDL_DestroyWindow(window);
-    SDL_GameControllerClose(controller);
+    SDL_DestroyRenderer(renderer);
+    if(gamepadConnected){
+        SDL_GameControllerClose(controller);
+    }
     SDL_Quit();
     return 0;
 }
