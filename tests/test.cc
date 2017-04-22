91e3c04ee6aede81e5bbfa4422a1a52fede3a688
adolan5 <then3rd3.14@gmail.com>
2017-04-22 14:50:21 -0600

    Yup, rendering has been successfully handed off and no longer takes place in test.cc. Yay organization!

--- tests/test.cc
+++ tests/test.cc
@@ -10,11 +10,8 @@ using namespace std;
 
 //Function declarations
 void openGamepad();
-bool loadTexture(string image, GLuint &textureID);
 int initialize();
 void quitGame();
-bool initializeGL();
-void glRender();
 void playGame();
 void generatePlatforms(vector<pair<int, int> > coordPairs); //TODO: Integration with Kinect stuff!
 int collision();
@@ -26,13 +23,6 @@ static const int WINDOW_WIDTH = 640;
 //Pointers for our window and renderer, and controller
 static SDL_Window *window;
 static SDL_GameController *controller;
-
-//OpenGL context and other variables
-// static SDL_GLContext gameContext;
-// static GLuint backgroundTexture = 0;
-// static GLuint playerTexture = 0;
-// static GLuint platformTexture = 0;
-
 //Controller deadzone
 static const int deadzone = 8000;
 //An event to be polled
@@ -69,41 +59,17 @@ int initialize(){
         return 1;
     }
     
-    
-    // Setting some SDL_GL attributes
-    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-    
     //Creating a window
     window = SDL_CreateWindow("ShadowPuppet", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
     if(window == NULL){
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to create window! Error: %s\n", SDL_GetError());
         return 1;
     }
-    
-    //Create OpenGL context
-    // gameContext = SDL_GL_CreateContext(window);
-    // if(gameContext == NULL){
-        // SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to create OpenGL context. Error: %s\n", SDL_GetError());
-        // return 1;
-    // }
-    
-    //Initialize OpenGL
-    // if(!initializeGL()){
-        // SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to initialize OpenGL! Error: %s\n", SDL_GetError());
-        // return 1;
-    // }
+	
 	if(!renderer.initializeGL(window)){
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to create OpenGL context. Error: %s\n", SDL_GetError());
 		return 1;
 	}
-    
-    //Initializing SDL_image
-    // int image = IMG_Init(IMG_INIT_PNG);
-    // if((image & IMG_INIT_PNG) != IMG_INIT_PNG){
-		// SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to initialize SDL_image! Error: %s\n", IMG_GetError());
-		// return 1;
-    // }
 	
     //Setting player initial position
     player.x = 0;
@@ -116,109 +82,15 @@ int initialize(){
     return 0;
 }
 
-//Function to start up OpenGL via OpenGL code
-/* bool initializeGL(){
-    // Setting up the orthographic coordinate system
-	glOrtho(0.0, WINDOW_WIDTH, WINDOW_HEIGHT, 0.0, 1.0, -1.0);
-	
-    // These flags must be enabled for transparency of PNGs to work
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	
-    // Loading the textures we're using
-    if(!loadTexture(string("../images/marblePng.png"), backgroundTexture)){
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to load texture from string! Error: %s\n", SDL_GetError());
-        return false;
-    }
-    if(!loadTexture(string("../images/cam.png"), playerTexture)){
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to load texture from string! Error: %s\n", SDL_GetError());
-        return false;
-    }
-    if(!loadTexture(string("../images/Platform.png"), platformTexture)){
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to load platform texture from string! Error: %s\n", SDL_GetError());
-		return false;
-	}
-    return true;
-} */
-
 //Function to shut down all libraries and exit
 void quitGame(){
-    //Delete textures now that we're done with them (and avoiding a very bad memory leak!)
-    // glDeleteTextures(1, &backgroundTexture);
-    // glDeleteTextures(1, &playerTexture);
-	// glDeleteTextures(1, &platformTexture);
-    // SDL_GL_DeleteContext(gameContext);
     SDL_DestroyWindow(window);
     if(gamepadConnected){
         SDL_GameControllerClose(controller);
     }
-    //IMG_Quit();
     SDL_Quit();
+	//Note: ShadowRenderer's dtor takes care of destroying textures and shutting down SDL_Image
 }
-
-/*
-Function that takes an image path, creates a surface from the image, and then a texture from the surface via OpenGL
-Params: the path to the image we're using as a c++ string
-*/
-// bool loadTexture(string image, GLuint &textureID){
-    //Loading SDL surface from png via SDL_image
-    // SDL_Surface *surface = IMG_Load(image.data());
-    // if(!surface){
-        // SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't load surface from image: %s Error: %s\n", image.data(), SDL_GetError());
-        // return false;
-    // }
-    // glGenTextures(1, &textureID);
-    //Binding to the texture
-    // glBindTexture(GL_TEXTURE_2D, textureID);
-    //Setting up the texture's settings
-    // glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    // glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // Actually creating the texture
-    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
-    // Freeing the SDL surface, since we don't need it anymore
-    // SDL_FreeSurface(surface);
-    // return true;
-// }
-
-/*
-Function that does the OpenGL side of rendering textures that are loaded from images
-*/
-/* void glRender(){
-    // Clear the context first
-    glClear(GL_COLOR_BUFFER_BIT);
-    // Need this to be enabled for this portion
-    glEnable(GL_TEXTURE_2D);
-    // Render background texture onto target
-    glBindTexture(GL_TEXTURE_2D, backgroundTexture);
-    glBegin(GL_QUADS);
-        glTexCoord2f(0, 0); glVertex2f(0, 0);                           //Top left
-        glTexCoord2f(1, 0); glVertex2f(WINDOW_WIDTH, 0);                //Top right
-        glTexCoord2f(1, 1); glVertex2f(WINDOW_WIDTH, WINDOW_HEIGHT);    //Bottom right
-        glTexCoord2f(0, 1); glVertex2f(0, WINDOW_HEIGHT);               //Bottom left
-    glEnd();
-    // Render player
-    glBindTexture(GL_TEXTURE_2D, playerTexture);
-    glBegin(GL_QUADS);
-        glTexCoord2f(0,0); glVertex2f(player.x,player.y);
-        glTexCoord2f(1,0); glVertex2f((player.x + player.w),player.y);
-        glTexCoord2f(1,1); glVertex2f((player.x + player.w),(player.y + player.h));
-        glTexCoord2f(0,1); glVertex2f(player.x,(player.y + player.h));
-    glEnd();
-	// Render platform(s) if they're present
-	if(platformsPresent){
-		for(int i = 0; i < numPlatforms; i++){
-			glBindTexture(GL_TEXTURE_2D, platformTexture);
-			glBegin(GL_QUADS);
-				glTexCoord2f(0,0); glVertex2f(platforms[i].x,platforms[i].y);
-				glTexCoord2f(1,0); glVertex2f((platforms[i].x + platforms[i].w),platforms[i].y);
-				glTexCoord2f(1,1); glVertex2f((platforms[i].x + platforms[i].w),(platforms[i].y + platforms[i].h));
-				glTexCoord2f(0,1); glVertex2f(platforms[i].x,(platforms[i].y + platforms[i].h));
-			glEnd();
-		}
-	}
-    // Done with rendering, disable this flags
-    glDisable(GL_TEXTURE_2D);
-} */
 
 //Function to open a gamepad
 void openGamepad(){
